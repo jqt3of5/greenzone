86c78bf0419aa56f6e1029281456fc88d0f35ee6
John Todd <jqt3of5@gmail.com>
2013-12-08 00:42:35 -0700

    Added the spotify server

---
+++ spotify/spotifyserver/SpotifyClientTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace SpotifyClientTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("localhost", 8012);
+
+            string command = "[{\"command\":\"NextTrack\"}]";
+
+            client.GetStream().Write(Encoding.UTF8.GetBytes(command),0,command.Length);
+        }
+    }
+}
