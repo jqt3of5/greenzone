68b4269ff35e5269aa0a31b9d95407adcd4a0afa
John Todd <jqt3of5@gmail.com>
2014-04-17 22:33:34 -0600

    Adding some tools, and updating the login page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,7 +6,9 @@ var toddAtHomeApp = angular.module('toddAtHomeApp', ['ngRoute']);
 toddAtHomeApp.controller('MenuController', function($scope, $location) {
 	$scope.menuItems = [
 	{"header" : "Cloud Services", "routeArg" : "cloud"},
-	{"header" : "Zone Management", "routeArg" : "zone"}
+	{"header" : "Zone Management", "routeArg" : "zone"},
+	{"header" : "SSH Management", "routeArg" : "ssh"},
+	{"header" : "GIT Repositories", "routeArg" : "git"}
 	];
 	
 	$scope.navigateToSubTab = function(routeArg) {
@@ -18,6 +20,11 @@ toddAtHomeApp.controller('ZoneController', function($scope) {
 });
 toddAtHomeApp.controller('CloudController', function($scope) {
 });
+toddAtHomeApp.controller('SshController', function($scope) {
+});
+toddAtHomeApp.controller('GitController', function($scope) {
+});
+
 
 toddAtHomeApp.config(['$routeProvider', 
 	function($routeProvider) {
@@ -26,11 +33,19 @@ toddAtHomeApp.config(['$routeProvider',
 				templateUrl: 'partials/zone.html',
 				controller: 'ZoneController'
 				}).
-				when('/cloud', {
-					templateUrl: 'partials/cloud.html',
-					controller: 'CloudController'
+			when('/cloud', {
+				templateUrl: 'partials/cloud.html',
+				controller: 'CloudController'
 				}).
-				otherwise({
-					redirectTo: '/zone'
-					});
+			when('/ssh', {
+				templateUrl: 'partials/ssh.html',
+				controller: 'SshController'
+				}).
+			when('/git', {
+				templateUrl: 'partials/git.html',
+				controller: 'GitController'
+			}).
+			otherwise({
+				redirectTo: '/cloud'
+			});
 	}]);
--- www/login/login.php
+++ www/login/login.php
@@ -1,18 +1,22 @@
 <html>
     <head>
+		<link rel="stylesheet" href="css/login.css">
     </head>
 <body>
     <center>
-    <h1>You made it to the login!</h1><br>
-    <?php if (isset($_GET['error'])){ echo $_GET['error'] . '<br>'; } ?>
-    <form action='login-verify.php' method = 'post'>
-         User Name: <input type='text' name='username'><br>
-         Password: <input type='password' name='password'><br>
-         <input type='submit' value='Submit'>
-    </form>
+		<h1> TODD @ HOME </h1><br>
+		<p> A Place for Web Development</p>
+	</center>
+	<div id="login">
+		<form action='login-verify.php' method = 'post'>
+			User Name: <input type='text' name='username'><br>
+			Password: <input type='password' name='password'><br>
+			<input type='submit' value='Submit'>
+		</form>
+		<?php if (isset($_GET['error'])){ echo $_GET['error']; } ?>
+	</div>
     <br><br>
-    Dont have an account? <a href="signup.php" >Click Here</a>
-			
-    </center>
+    <div id="signup">Dont have an account? <a href="signup.php" >Click Here</a> </div>
+			    
 </body>
 </html>
